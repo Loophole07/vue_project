bd9f1e5beca55dbc79efae2040144e600e2c9d0a
Samip Rawal <samiprawal@gmail.com>
2026-01-12 19:27:36 +0545

    feat: finish shopping cart implementation in Vue

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
   ],
+
+  // 🔴 REQUIRED FOR GITHUB PAGES
+  base: '/vue_project/',
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
