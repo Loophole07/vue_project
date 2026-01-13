eedba5b88597b0f0c39c451077d91def41a36b01
Samip Rawal <samiprawal@gmail.com>
2026-01-13 13:05:45 +0545

    feat(deploy): deploy Vue project to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
-  base: '/vue_project/',   // important for GitHub Pages
-  plugins: [
-    vue(),
-    vueDevTools()
-  ],
+  base: '/vue_project/',
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
