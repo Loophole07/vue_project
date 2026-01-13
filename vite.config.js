2819eae4e9e28757adae6b643a22858a252ce99d
Samip Rawal <samiprawal@gmail.com>
2026-01-13 14:30:30 +0545

    fix: correct some mistakes

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,11 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/vue_project/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  
 })
